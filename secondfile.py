647ac57bb94d6117f4f5533ee15e166eb6755ddd
ahmedkhalid-tech <134700087+ahmedkhalid-tech@users.noreply.github.com>
2023-05-26 16:20:50 +0500

    Update secondfile.py

--- secondfile.py
+++ secondfile.py
@@ -7,7 +7,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 
-# Load the dataset
+# Load the dataset -- 
 dataset_path = 'path_to_dataset_folder/drowsiness_dataset.csv'
 data = pd.read_csv(dataset_path)
 
